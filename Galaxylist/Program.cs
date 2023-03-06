86f222bc7b383790af2591ff3f386c24d07a5265
mrtn <mrtngamper@gmail.com>
2023-03-06 19:24:48 +0100

    CoordinateConverter

--- Galaxylist/Program.cs
+++ Galaxylist/Program.cs
@@ -1,4 +1,5 @@
 global using FastEndpoints;
+using Galaxylist.Util;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFastEndpoints();
@@ -14,4 +15,11 @@ app.UseFastEndpoints(config =>
 	}
 );
 
-app.Run();
+// 150 az
+//CoordinateConverter.ConvEqToZen(DateTime.Today + new TimeSpan(21,0,0), 11.93, 46.8, 152.1, 11.9667);
+CoordinateConverter.ConvEqToZen(DateTime.Today + new TimeSpan(17,0,0), 11.93, 46.8, 
+	CoordinateConverter.HourMinSecToDegree(5,15,38.8), 
+	CoordinateConverter.DegreeMinSecToDegree(-8,10,39)
+	);
+
+//app.Run();
